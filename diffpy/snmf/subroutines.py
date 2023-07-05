a8745f7fab74c7655e010879d4d0d8600b31990d
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-05 07:21:34 -0500

    update

--- diffpy/snmf/subroutines.py
+++ diffpy/snmf/subroutines.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.interpolate
 
 
 def objective_function(residual_matrix, stretching_factor_matrix, smoothness, smoothness_term, component_matrix,
@@ -13,18 +14,22 @@ def objective_function(residual_matrix, stretching_factor_matrix, smoothness, sm
       patterns.
 
     stretching_factor_matrix: 2d array like
-      The matrix containing the stretching factors of the calculated component signal. Has dimensions
+      The matrix containing the stretching factors of the calculated component signal. Has dimensions K x M where K is
+      the amount of components and M is the number of experimental PDF/XRD patterns.
 
     smoothness: float
-      The coefficient of the smoothness term which
+      The coefficient of the smoothness term which determines
 
 
     smoothness_term: 2d array
       The term that ensures that smooth changes in the component signals are favored.
 
-    component_matrix:
+    component_matrix: 2d array
+      The matrix containing the calculated component signals of the experimental PDF/XRD patterns. Has dimesions R x K
+      where R is the signal length and K is the number of component signals.
 
-    sparsity
+    sparsity: float
+      The parameter
 
     Returns
     -------
@@ -32,3 +37,21 @@ def objective_function(residual_matrix, stretching_factor_matrix, smoothness, sm
     """
     return .5 * np.linalg.norm(residual_matrix, 'fro') ** 2 + .5 * smoothness * np.linalg.norm(
         smoothness_term @ stretching_factor_matrix.T, 'fro') ** 2 + sparsity * np.sum(np.sqrt(component_matrix))
+
+
+def get_stretched_component(stretching_factor, component, grid_vector):
+    spline = scipy.interpolate.UnivariateSpline(grid_vector, component, k=2)
+    stretched_component = spline.__call__(grid_vector / stretching_factor)
+    stretched_component_first_derivative = spline.__call__(grid_vector / stretching_factor, nu=1)
+    stretched_component_second_derivative = spline.__call__(grid_vector / stretching_factor, nu=2)
+
+
+def update_weights_matrix(component_amount, signal_length, stretching_factor_matrix, component_matrix, data_input,
+                          moment_amount):
+    weight = np.zeros(component_amount)
+    for i in range(moment_amount):
+        gram_matrix = np.zeros(signal_length, component_amount)
+        for n in range(component_amount):
+            gram_matrix[:, n] = get_stretched_component(stretching_factor_matrix[n, i], component_matrix[:, n])
+
+        weight
