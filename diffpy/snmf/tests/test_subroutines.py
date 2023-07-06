148ddccc79bf2eb6e2626972ef0131baa0d6a656
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-06 09:24:26 -0500

    added test for floats

--- diffpy/snmf/tests/test_subroutines.py
+++ diffpy/snmf/tests/test_subroutines.py
@@ -3,8 +3,16 @@ from diffpy.snmf.subroutines import objective_function, get_stretched_component
 
 to = [
     ([[[1, 2], [3, 4]], [[5, 6], [7, 8]], 1e11, [[1, 2], [3, 4]], [[1, 2], [3, 4]], 1], 2.574e14),
+    # Positive square matrices which large smoothness
     ([[[11, 2], [31, 4]], [[5, 63], [7, 18]], .001, [[21, 2], [3, 4]], [[11, 22], [3, 40]], 1], 650.4576),
+    # Positive square matrix with small smoothness
     ([[[1, 2], [3, 4]], [[5, 6], [7, 8]], 1e11, [[1, 2], [3, 4]], [[1, 2], [3, 4]], 0], 2.574e14),
+    # Positive square matrices which sparseness of 0
+    # Rectangular positive matrices
+    # Negative Matrices
+    # Positive matrices of floats
+    # Negative matrices of floats
+    # All zero matrices
 
 ]
 
@@ -17,7 +25,8 @@ def test_objective_function(to):
 
 
 tgsc = [
-    ([.2220, [1, 2, 3, 4, 5, 6, 7, 8, 9], [0,1,2,3,4,5,6,7,8]], ([1, 5.5045, 0, 0, 0, 0, 0, 0, 0],[0,-20.2906,0,0,0,0,0,0,0],[182.7978,0,0,0,0,0,0,0,0]))
+    ([.2220, [1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8]],
+     ([1, 5.5045, 0, 0, 0, 0, 0, 0, 0], [0, -20.2906, 0, 0, 0, 0, 0, 0, 0], [182.7978, 0, 0, 0, 0, 0, 0, 0, 0]))
 ]
 
 
