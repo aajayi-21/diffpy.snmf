444a4e6a99f1834d118fae7f1cf0f166ee509197
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-17 09:17:40 -0500

    updated

--- diffpy/snmf/subroutines.py
+++ diffpy/snmf/subroutines.py
@@ -78,6 +78,7 @@ def get_stretched_component(stretching_factor, component, signal_length):
     normalized_grid = np.arange(signal_length)
     return np.interp(normalized_grid / stretching_factor, normalized_grid, component, left=0, right=0)
 
+
 def update_weights_matrix(component_amount, signal_length, stretching_factor_matrix, component_matrix, data_input,
                           moment_amount, weights_matrix, method):
     """Updates the weights matrix.
@@ -201,16 +202,24 @@ def reconstruct_data(stretching_factor_matrix, component_matrix, weight_matrix, 
                                                           component_matrix[:, component], signal_length)
             stretched_component_series.append(stretched_component)
     stretched_component_series = np.column_stack(stretched_component_series)
-    print(stretched_component_series)
+
     reconstructed_data = []
     moment = 0
     for block in range(0, moment_amount, component_amount):
         component_block = stretched_component_series[:, block:block + component_amount]
-        print(component_block)
         for component in range(component_amount):
             component_block[:, component] = component_block[:, component] * weight_matrix[component, moment]
             reconstructed_data.append(np.sum(component_block, axis=0))
-        print(component_block)
-        print(reconstructed_data)
         moment += 1
     return np.column_stack(reconstructed_data)
+
+
+def funregu(signal_length, moment_amout, component_amount, data_input):
+    AX = reconstruct_data()
+    RA = np.reshape(np.sum(np.reshape(AX, (signal_length * moment_amout, component_amount)), axis=1),
+                    (signal_length, moment_amout)) - data_input
+    return objective_function()
+
+
+def update_stretching_matrix():
+    scipy.optimize.minimize()
